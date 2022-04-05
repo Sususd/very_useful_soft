48e9f188e5f51a018e20ce2cf0a42bdecca0ed0e
Sususd <99991485+Sususd@users.noreply.github.com>
2022-04-05 21:15:36 +0300

    Add files via upload

--- python_works/window.py
+++ python_works/window.py
@@ -3,6 +3,7 @@ import colorsys
 from tkinter import *
 import tkinter as tk
 from pygame import mixer
+import pygame
 import os
 import sys
 from tkinter import ttk
@@ -10,8 +11,15 @@ from tkinter.messagebox import showinfo
 import tkinter.font as tkFont
 import time
 import fnmatch
+from tkinter import filedialog
+from mutagen.mp3 import MP3
+import tkinter.filedialog as filedialog
+from tkinter.messagebox import showinfo
+import tkinter.ttk as ttk
+import virtualenv
 
 
+pygame.init()
 mixer.init()
 
 clicks = 0                     #кликер в названии окна
@@ -173,82 +181,175 @@ def click_b7():
         window.mainloop()
 
 def music():
-	window = tk.Tk()
-	window.title("Music Player")
-	window.geometry("800x520")
-	window.config(bg = "black")
+	pygame.init()
+	mixer.init()
 
-	rootpath = "Музыка"
-	pattern = "*.mp3"
+	def play_time():
+        	current_time = mixer.music.get_pos() / 1000
+        	converted_current_time = time.strftime('%H:%M:%S', time.gmtime(current_time))
+        	status_bar.config(text = converted_current_time)
 
-	def select():
-		label.config(text = listBox.get("anchor"))
-		mixer.music.load(rootpath + "//" + listBox.get("anchor"))
-		mixer.music.play()
+        	status_bar.after(1000, play_time)
 
-	def stop():
-		mixer.music.stop()
-		listBox.select_clear("active")
+	def playsong():
+        	currentsong = playlist.get(ACTIVE)
+        	print(currentsong)
+        	mixer.music.load(currentsong)
+        	mixer.music.play()
+
+        	play_time()
+
+	def pausesong():
+        	mixer.music.pause()
+
+	def stopsong():
+        	mixer.music.stop()
+
+	def resumesong():
+        	mixer.music.unpause()
 
 	def play_next():
-		next_song = listBox.curselection()
-		next_song = next_song[0] + 1
-		next_song_name = listBox.get(next_song)
-		label.config(text = next_song_name)
-		mixer.music.load(rootpath + "//" + next_song_name)
-		mixer.music.play()
-		listBox.select_clear(0, "end")
-		listBox.activate(next_song)
-		listBox.select_set(next_song)
+        	next_song = playlist.curselection()
+        	next_song = next_song[0] + 1
+        	next_song_name = playlist.get(next_song)
+        	label.config(text = next_song_name)
+        	mixer.music.load(rootpath + next_song_name)
+        	mixer.music.play()
+        	playlist.select_clear(0, "end")
+        	playlist.activate(next_song)
+        	playlist.select_set(next_song)
 
 	def play_prev():
-		next_song = listBox.curselection()
-		next_song = next_song[0] - 1
-		next_song_name = listBox.get(next_song)
-		label.config(text = next_song_name)
-		mixer.music.load(rootpath + "//" + next_song_name)
-		mixer.music.play()
-		listBox.select_clear(0, "end")
-		listBox.activate(next_song)
-		listBox.select_set(next_song)
+        	next_song = playlist.curselection()
+        	next_song = next_song[0] - 1
+        	next_song_name = playlist.get(next_song)
+        	label.config(text = next_song_name)
+        	mixer.music.load(rootpath + next_song_name)
+        	mixer.music.play()
+        	playlist.select_clear(0, "end")
+        	playlist.activate(next_song)
+        	playlist.select_set(next_song)
 
-	def pause_song():
-		if pauseButton["text"] == "Pause":
-			mixer.music.pause()
-			pauseButton["text"] = "Play"
-		else:
-			mixer.music.unpause()
-			pauseButton["text"] = "Pause"
+	rootpath = ""
+	pattern = "*.mp3"
 
-	listBox = tk.Listbox(window, fg = "cyan",  bg = "black", width = 100, font = ("ds-digital", 14))
-	listBox.pack(padx = 15, pady = 15)
+	window = Tk()
+	window.geometry("453x250")
+	window.title("Music player")
 
-	label = tk.Label(window, text = "", bg = "black", fg = "yellow", font = ("ds-digital", 18))
-	label.pack(pady = 15)
-	top = tk.Frame(window, bg = "black")
-	top.pack(padx = 10, pady = 5, anchor = "center")
+	#icon = PhotoImage(file = ' ')
+	#window.iconphoto(True, icon)
 
-	prev = tk.Button(window, text = "Попередній", command = play_prev)
-	prev.pack(pady = 15, in_ = top, side = "left")
+	#bg = PhotoImage(file = ' ')
+	#label = Label( window,
+	#	image = bg)
+	#label.place(x=0, y=0)
 
-	stop = tk.Button(window, text = "Стоп", command = stop)
-	stop.pack(pady = 15,in_ = top, side = "left" )
+	def volume(x):
+        	mixer.music.set_volume(w.get())
 
-	play = tk.Button(window, text = "Грати", command = select)
-	play.pack(pady = 15,in_ = top, side = "left" )
+	w = ttk.Scale(window,
+        	from_ = 0,
+        	to = 1,
+        	command = volume,
+        	orient = HORIZONTAL)
+	w.pack()
+	w.place(x=5, y=190)
 
-	pause = tk.Button(window, text = "Пауза", command = pause_song)
-	pause.pack(pady = 15,in_ = top, side = "left" )
+	playlist = Listbox(window,
+        	selectmode=SINGLE,
+		width = 61,
+        	height = 8,
+        	foreground = 'white',
+        	background = 'black')
+	playlist.pack()
+	playlist.place(x=6, y=10)
 
-	next = tk.Button(window, text = "Наступний", command = play_next)
-	next.pack(pady = 15,in_ = top, side = "left" )
+	os.chdir('/home/manjus/Музыка')
+	song = os.listdir()
 
-	for root, dirs, files, in os.walk(rootpath):
-		for filename in fnmatch.filter(files, pattern):
-			listBox.insert("end", filename)
+	for s in song:
+        	playlist.insert(END,s)
 
+	btn = Button(window,
+        	text = '⫸',
+        	command = playsong,
+        	width = 5,
+        	background = '#458')
+	btn.pack()
+	btn.place(x=130, y=157)
+
+	btn = Button(window,
+        	text = '||',
+        	command = pausesong,
+        	width = 5,
+        	background = '#390')
+	btn.pack()
+	btn.place(x=195, y=187)
+
+	btn = Button(window,
+        	text = '⋫',
+        	command = stopsong,
+        	width = 5,
+        	background = '#458')
+	btn.pack()
+	btn.place(x=130, y=187)
+
+	btn = Button(window,
+        	text = '⊳',
+        	command = resumesong,
+        	width = 5,
+        	background = '#390')
+	btn.pack()
+	btn.place(x=195, y=157)
+
+	btn = Button(window,
+        	text = '▶▶',
+        	command = play_next,
+        	width = 5,
+        	background = 'purple')
+	btn.pack()
+	btn.place(x=260, y=157)
+
+	btn = Button(window,
+        	text = '◀◀',
+        	command = play_prev,
+        	width = 5,
+        	background = 'purple')
+	btn.pack()
+	btn.place(x=260, y=187)
+
+	def exit():
+        	window.destroy()
+
+	btn = Button(window,
+        	text = 'exit',
+        	command = exit,
+        	background = '#900')
+	btn.pack()
+	btn.place(x=403, y=180)
+
+	status_bar = Label(window,
+        	text = '',
+        	bd = 1,
+        	relief = GROOVE,
+		anchor = E,
+        background = 'black',
+        foreground = 'white')
+	status_bar.pack(fill = X, side = BOTTOM, ipady = 2)
+
+	window["bg"]="#203"
 	window.resizable(False, False)
 	window.mainloop()
+
+def play_next():
+        next_song = playlist.curselection()
+        next_song = next_song[0] + 1
+        next_song_name = playlist.get(next_song)
+        label.config(text = next_song_name)
+        mixer.music.load(rootpath + next_song_name)
+        mixer.music.play()
+        playlist.select_clear(0, "end")
 
 class HoverButton(tk.Button):                          #для подмены кнопки
         def __init__(self, master, **kw):
@@ -284,6 +385,14 @@ class HoverButton1(tk.Button):
 window = Tk()                      #окно
 window.geometry("300x800")
 
+icon = PhotoImage(file = 'logo.png')
+window.iconphoto(True, icon)
+
+bg = PhotoImage(file = "background1.png")
+label = Label( window,
+	image = bg)
+label.place(x=0, y=0)
+
 window.bind('<Motion>', move)      #для слежки за курсором
 
 label = Label(window,
@@ -305,7 +414,7 @@ btn = Button(window,
         command = click_b3,
         font = ("Helvetica", 18))        #цвет кнопки
 btn.pack()
-btn.place(y=480, x=25)
+btn.place(y=660, x=25)
 
 #sloadimage = PhotoImage(file = "b2.png")
 
@@ -318,7 +427,7 @@ btn = Button(window,
         command = click_b5,
         font = ("Helvetica", 18))        #цвет кнопки
 btn.pack()
-btn.place(x=25, y=370)
+btn.place(x=25, y=550)
 
 def Close():
         window.destroy()
@@ -333,7 +442,7 @@ classButton = HoverButton1(window,
         font = ("Helvetica", 16),
         command = Close)
 classButton.pack()
-classButton.place(x=165, y=740)
+classButton.place(x=188, y=740)
 
 classButton = HoverButton1(window,
         text = "About us",
@@ -357,7 +466,7 @@ btn = Button(window,
         background = "#458",
         font = ("Helvetica", 18))
 btn.pack()
-btn.place(x=25, y=260)
+btn.place(x=25, y=440)
 
 def epic_moment():
 	t = turtle.Turtle()
@@ -389,7 +498,7 @@ btn = Button(window,
         background = "#458",
         font = ("Helvetica", 18))
 btn.pack()
-btn.place(x=25, y=150)
+btn.place(x=25, y=330)
 
 btn = Button(window,
 	text = "Player",
@@ -399,7 +508,7 @@ btn = Button(window,
 	font = ("Helvetica", 18),
 	command = music)
 btn.pack()
-btn.place(x=25, y=50)
+btn.place(x=25, y=220)
 
 window.resizable(False, False)     #заморозка разрешения окна
 window["bg"]="#203"
